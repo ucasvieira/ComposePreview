ee146389e220b1d34ca607562557060634a3082b
ucasvieira <lucas.souza1012@etec.sp.gov.br>
2023-08-15 10:34:41 -0300

    Composables design

--- app/src/main/java/com/example/composeupdate/MainActivity.kt
+++ app/src/main/java/com/example/composeupdate/MainActivity.kt
@@ -8,19 +8,33 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialogDefaults.shape
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,131 +68,75 @@ private fun app(){
             verticalArrangement = Arrangement.SpaceEvenly,
             horizontalAlignment = Alignment.CenterHorizontally
         ){
-            Greeting("Jetpack")
-            ActionButton(
-                text = "Debug",
-                buttonColors = DebugButtonColors(),
-                modifier = Modifier.fillMaxWidth(0.5f)
-            ){
-                Log.i(TAG, "App: Clicou em DEBUG!")
-            }
-            ActionButton(
-                text = "Info",
-                buttonColors = InfoButtonColors(),
-                modifier = Modifier.fillMaxWidth(0.5f)
-            ) {
-                Log.i(TAG, "App: Clicou em INFO!")
-            }
-            ActionButton(
-                text = "Warning",
-                buttonColors = WarningButtonColors(),
-                modifier = Modifier.fillMaxWidth(0.5f)
-            ) {
-                Log.i(TAG, "App: Clicou em WARNING!")
-            }
-            ActionButton(
-                text = "Error",
-                buttonColors = ErrorButtonColors(),
-                modifier = Modifier.fillMaxWidth(0.5f)
-            ) {
-                try{
-                    throw RuntimeException("Clicou em Error!")
-                }catch (ex: Exception){
-                    Log.e(TAG,"${ex.message}")
-                }
-            }
+            ElevatedButtonSample()
+            OutlinedButtonSample()
+            CardSample()
         }
     }
 }
 
 @Composable
-fun ActionButton(
-    text: String,
-    buttonColors: ButtonColors =ButtonDefaults.buttonColors(),
-    modifier: Modifier = Modifier,
-    block: () -> Unit
-){
-    ElevatedButton(
-        onClick =block,
-        shape = RoundedCornerShape(5.dp),
-        colors =buttonColors,
-        modifier = modifier
-    ) {
-        Text(text = text)
+fun CardSample() {
+    Card(Modifier.size(width = 180.dp, height = 100.dp)) {
+       CheckboxWithTextSample()
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Aula de $name!",
-        style = MaterialTheme.typography.bodyLarge.copy(
-            fontWeight = FontWeight.Bold
-        ),
-        color = MaterialTheme.colorScheme.secondary
-    )
+fun CheckboxWithTextSample() {
+    val (checkedState, onStateChange) = remember { mutableStateOf(true) }
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .height(56.dp)
+            .toggleable(
+                value = checkedState,
+                onValueChange = { onStateChange(!checkedState) },
+                role = Role.Checkbox
+            )
+            .padding(horizontal = 16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Checkbox(
+            checked = checkedState,
+            onCheckedChange = null // null recommended for accessibility with screenreaders
+        )
+        Text(
+            text = "Dale no Dole?",
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier.padding(start = 16.dp)
+        )
+    }
 }
 
-@Preview(showBackground = true, widthDp = 200, heightDp = 300)
+@Composable
+fun ElevatedButtonSample() {
+    ElevatedButton(
+        onClick = {},
+        modifier = Modifier ,
+        enabled = true,
+        shape = ButtonDefaults.elevatedShape,
+        colors = ButtonDefaults.elevatedButtonColors(),
+        elevation = ButtonDefaults.elevatedButtonElevation(),
+        border = null,
+        contentPadding = ButtonDefaults.ContentPadding,
+    ) {
+        Text("Morrer agora");
+
+    }
+}
+
+
+
+@Composable
+fun OutlinedButtonSample() {
+    OutlinedButton(onClick = { /* Do something! */ }) { Text("Morrer Depois") }
+}
+
+
+
+@Preview(showBackground = true, widthDp = 250, heightDp = 350)
 @Composable
 fun appPreview(){
     app()
-}
-
-@Preview(showBackground = true, widthDp = 120)
-@Composable
-fun ButtonDebugPreview(){
-    ActionButton(
-        text = "Debug",
-        buttonColors = DebugButtonColors(),
-        modifier = Modifier.fillMaxWidth(0.5f)
-    ){
-        Log.i(TAG, "App: Clicou em DEBUG!")
-    }
-}
-
-@Preview(showBackground = true, widthDp = 120)
-@Composable
-fun ButtonInfoPreview(){
-    ActionButton(
-        text = "Info",
-        buttonColors = InfoButtonColors(),
-        modifier = Modifier.fillMaxWidth(0.5f)
-    ) {
-        Log.i(TAG, "App: Clicou em INFO!")
-    }
-}
-
-@Preview(showBackground = true, widthDp = 120)
-@Composable
-fun ButtonWarningPreview(){
-    ActionButton(
-        text = "Warning",
-        buttonColors = WarningButtonColors(),
-        modifier = Modifier.fillMaxWidth(0.5f)
-    ) {
-        Log.i(TAG, "App: Clicou em WARNING!")
-    }
-}
-
-@Preview(showBackground = true, widthDp = 120)
-@Composable
-fun ButtonErrorPreview(){
-    ActionButton(
-        text = "Error",
-        buttonColors = ErrorButtonColors(),
-        modifier = Modifier.fillMaxWidth(0.5f)
-    ) {
-        try{
-            throw RuntimeException("Clicou em Error!")
-        }catch (ex: Exception){
-            Log.e(TAG,"${ex.message}")
-        }
-    }
-}
-
-@Preview
-@Composable
-fun GreetingPreview(){
-    Greeting("lucas")
 }
